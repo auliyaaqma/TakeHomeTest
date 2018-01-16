9388bea6d6393cb391800a107b3807e5183973e4
fadhlanariq <fadhlan.ariq.tif16@polban.ac.id>
2018-01-17 00:50:04 +0700

    tambah komentar di props.put

--- Thread/src/TakeHomeTest/SendMail1.java
+++ Thread/src/TakeHomeTest/SendMail1.java
@@ -48,8 +48,8 @@ public class SendMail1 implements Runnable { // didalam kelas sendemail mengimpl
         
         Properties props = new Properties();
         props.put("mail.smtp.starttls.enable", "true"); 
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.auth", "true"); // Authentikasi : mengisyaratkan Java Mail untuk melakukan authentikasi menggunakan username dan password untuk account email yang bersangkutan.
+        props.put("mail.smtp.host", "smtp.gmail.com"); // Alamat host SMTP
         props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props,
